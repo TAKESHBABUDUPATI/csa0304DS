ad70b30ffa03de954bf660d6afb462cb820bcf31
Takesh Babu <113873006+TAKESHBABUDUPATI@users.noreply.github.com>
2023-02-06 10:41:12 +0530

    Create 6.matrixmult.c

---
+++ 6.matrixmult.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+int main()
+{
+    int a[10][10],b[10][10],c[10][10],n,i,j,k;
+    printf("ENTER THE VALUE OF N=");
+    scanf("%d",&n);
+    printf("ENTER THE ELEMENTS OF MATRIX A=\n");
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    printf("ENTER THE ELEMENTS OF MATRIX B=\n");
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            scanf("%d",&b[i][j]);
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            c[i][j]=0;
+            for(k=0;k<n;k++)
+            {
+                c[i][j] += a[i][j]*b[i][j];
+            }
+        }
+    }
+    printf("MATRIX MULTIPLICATION=\n");
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            printf("%d\t",c[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+
+
+}
